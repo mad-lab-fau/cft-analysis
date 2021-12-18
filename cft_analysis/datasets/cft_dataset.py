cc7f35234c34a470f654f656b168d7695b15616e
Robert Richer <robert.richer@fau.de>
2021-12-18 09:21:06 +0100

    added questionnaire and cortisol properties to dataset

--- cft_analysis/datasets/cft_dataset.py
+++ cft_analysis/datasets/cft_dataset.py
@@ -1,16 +1,17 @@
 import itertools
 from functools import cached_property, lru_cache
-from typing import Tuple, Optional, Sequence, Union, Dict
-
-import pandas as pd
+from typing import Optional, Sequence, Union, Dict
 
 import biopsykit as bp
+import pandas as pd
+from biopsykit.io import load_questionnaire_data, load_long_format_csv
+from biopsykit.utils.dataframe_handling import multi_xs
 from tpcp import Dataset
 
 from cft_analysis._types import path_t
 from cft_analysis.datasets.helper import load_ecg_raw_data_folder
 
-_cached_load_ecg_raw_data_folder = lru_cache(maxsize=4)(load_ecg_raw_data_folder)
+_cached_load_ecg_raw_data_folder = lru_cache(maxsize=5)(load_ecg_raw_data_folder)
 
 
 class CftDataset(Dataset):
@@ -72,3 +73,33 @@ class CftDataset(Dataset):
         if self.is_single(None):
             return data_dict[phase[0]]
         return data_dict
+
+    @property
+    def questionnaire(self):
+        if self.is_single(None):
+            raise ValueError("questionnaire data can not be accessed for individual phases!")
+        return self._load_questionnaire_data()
+
+    @property
+    def cortisol(self) -> pd.DataFrame:
+        return self._load_saliva_data("cortisol")
+
+    def _load_questionnaire_data(self) -> pd.DataFrame:
+        data_path = self.base_path.joinpath("questionnaire/processed/questionnaire_data.csv")
+
+        data = load_questionnaire_data(data_path)
+        subject_ids = self.index["subject"].unique()
+        data = data.loc[subject_ids]
+
+        condition = self.index["condition"].unique()
+        return multi_xs(data, condition, level="condition")
+
+    def _load_saliva_data(self, saliva_type: str) -> pd.DataFrame:
+        if self.is_single(["subject", "condition", "phase"]):
+            raise ValueError(f"{saliva_type} data can not be accessed for individual phases!")
+        data_path = self.base_path.joinpath(f"saliva/processed/{saliva_type}_samples.csv")
+        data = load_long_format_csv(data_path)
+
+        subject_ids = self.index["subject"].unique()
+        conditions = self.index["condition"].unique()
+        return multi_xs(multi_xs(data, subject_ids, level="subject"), conditions, level="condition")
